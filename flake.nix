667177b8eb7d4d129196a26b89cda96752e35bcf
Joshua Luckie <luckie.joshua.c@gmail.com>
2024-07-01 23:29:51 -0400

    Returning to normal since I think the name has to be correct, might just need to include in update

--- flake.nix
+++ flake.nix
@@ -16,7 +16,7 @@
     outputs = flake-utils.lib.eachSystem systems (system: let
       pkgs = nixpkgs.legacyPackages.${system};
     #pkgs = import nixpkgs {inherit system;};
-    pygamece = import ./default.nix {inherit system pkgs;};
+    #pygamece = import ./default.nix {inherit system pkgs;};
   in rec {
     # Packages that append the python version to the previous iteration, so python312-2.5.0 for tag 2.5.0
     # - default - python312 with latest package
