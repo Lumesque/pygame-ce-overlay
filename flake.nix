59a73903c1e7b0bcd74f5f472191c8b95f9ebef5
Joshua Luckie <luckie.joshua.c@gmail.com>
2024-07-01 22:57:26 -0400

    Adding in a default shell hopefully

--- flake.nix
+++ flake.nix
@@ -13,7 +13,17 @@
   in rec {
     # Packages that append the python version to the previous iteration, so python312-2.5.0 for tag 2.5.0
     # - default - python312 with latest package
-    packages.${system} = pygamece;
+    full_package = pkgs.symlinkJoin {
+      name = "pygame-ce";
+      paths = [
+        pygamece."python312-2.5.0"
+        (pkgs.python312.withPackages (pp: [
+          pp.ipython
+        ]))
+      ];
+      buildInputs = [ pkgs.makeWrapper ];
+    };
+    packages.${system}.default = full_package;
     #self.packages.${system}.default = pygamece.default;
     devShells.${system}.default = pkgs.mkShell {
       nativeBuildInputs = with pkgs; [
