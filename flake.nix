fc0aa41c46b8147ab1bf06682a85e2527cdc660d
Joshua Luckie <luckie.joshua.c@gmail.com>
2024-07-02 09:07:55 -0400

    Updating to include other default systems

--- flake.nix
+++ flake.nix
@@ -11,12 +11,8 @@
   };
   outputs = { self, nixpkgs , flake-utils, ...}@inputs: 
   let
-    system = "x86_64-linux";
-    systems = [system];
-    outputs = flake-utils.lib.eachSystem systems (system: let
+    outputs = flake-utils.lib.eachDefaultSystem (system: let
       pkgs = nixpkgs.legacyPackages.${system};
-    #pkgs = import nixpkgs {inherit system;};
-    #pygamece = import ./default.nix {inherit system pkgs;};
   in rec {
     # Packages that append the python version to the previous iteration, so python312-2.5.0 for tag 2.5.0
     # - default - python312 with latest package
