142cabec7890d241866c47df29aa4643d8247493
Joshua Luckie <luckie.joshua.c@gmail.com>
2024-07-01 22:43:30 -0400

    Updating what packages

--- flake.nix
+++ flake.nix
@@ -13,8 +13,8 @@
   in rec {
     # Packages that append the python version to the previous iteration, so python312-2.5.0 for tag 2.5.0
     # - default - python312 with latest package
-    packages.${system}.default = pygamece.default;
-    self.packages.${system}.default = pygamece.default;
+    packages.${system} = pygamece;
+    #self.packages.${system}.default = pygamece.default;
     devShells.${system}.default = pkgs.mkShell {
       nativeBuildInputs = with pkgs; [
         curl
