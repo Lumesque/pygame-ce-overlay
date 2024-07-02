b5c6d4ad4844a45a39c2fe7877361b7fb8e56ba5
Joshua Luckie <luckie.joshua.c@gmail.com>
2024-07-01 23:03:31 -0400

    Trying a bit different of a strategy

--- flake.nix
+++ flake.nix
@@ -8,32 +8,25 @@
   outputs = { self, nixpkgs , flake-utils, ...}@inputs: 
   let
     system = "x86_64-linux";
-    pkgs = import nixpkgs {inherit system;};
+    systems = [system];
+    outputs = flake-utils.lib.eachSystem systems (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    #pkgs = import nixpkgs {inherit system;};
     pygamece = import ./default.nix {inherit system pkgs;};
   in rec {
     # Packages that append the python version to the previous iteration, so python312-2.5.0 for tag 2.5.0
     # - default - python312 with latest package
-    full_package = pkgs.symlinkJoin {
-      name = "pygame-ce";
-      paths = [
-        pygamece."python312-2.5.0"
-        (pkgs.python312.withPackages (pp: [
-          pp.ipython
-        ]))
-      ];
-      buildInputs = [ pkgs.makeWrapper ];
-    };
-    packages.${system}.default = full_package;
+    packages = import ./default.nix {inherit system pkgs;};
     #self.packages.${system}.default = pygamece.default;
-    devShells.${system}.default = pkgs.mkShell {
+    devShells.default = pkgs.mkShell {
       nativeBuildInputs = with pkgs; [
         curl
         jq
         nurl
       ];
     };
-    overlays.default = final: prev: {
-      pygamepkgs = packages.${system};
-    };
-  };
+    devShell = self.devShells.${system}.default;
+    });
+  in
+    outputs;
 }
