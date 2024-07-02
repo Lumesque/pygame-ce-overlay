553a8281058f0c8272c37f488579d0d5f7f755c5
Joshua Luckie <luckie.joshua.c@gmail.com>
2024-07-01 22:25:38 -0400

    Adding in a single derivation

--- flake.nix
+++ flake.nix
@@ -9,10 +9,11 @@
   let
     system = "x86_64-linux";
     pkgs = import nixpkgs {inherit system;};
+    pygamece = import ./default.nix {inherit system pkgs;};
   in rec {
     # Packages that append the python version to the previous iteration, so python312-2.5.0 for tag 2.5.0
     # - default - python312 with latest package
-    packages.${system}.default = import ./default.nix {inherit system pkgs;};
+    packages.${system}.default = pygamece.default;
     devShells.${system}.default = pkgs.mkShell {
       nativeBuildInputs = with pkgs; [
         curl
