817cf1bd3599f59feaf9e50441b4f998a47cafa4
Joshua Luckie <luckie.joshua.c@gmail.com>
2024-07-01 22:09:44 -0400

    Adding in initial setup, deriving in multiple instantiations for pygamece

---
+++ default.nix
@@ -0,0 +1,51 @@
+{
+  pkgs ? import <nixpkgs> {},
+  system ? builtins.currentSystem,
+  python_vers ? ["python312" "python311" "python310"]
+}: let
+  inherit (pkgs) lib;
+  # Sources that has each ref needed to make a package
+  sources = builtins.fromJSON (builtins.readFile ./index.json);
+  # Sources that has branches, and tags
+  _tags = builtins.fromJSON (builtins.readFile ./sources.json);
+  first_tag = lib.lists.head _tags.tag;
+  versions = lib.lists.forEach python_vers (p: lib.attrsets.mapAttrs' (name: value:  lib.nameValuePair (p + "-" + name)
+        (
+          pkgs.python312Packages.buildPythonPackage rec {
+            pname = "pygame-ce";
+            version = name;
+            doCheck = false;
+            src = pkgs.fetchFromGitHub {
+              owner = value.owner;
+              repo = value.repo;
+              rev = name;
+              hash = value.hash;
+            };
+            buildInputs = [
+              (pkgs.python312.withPackages (pp: [
+                pp.setuptools
+                pp.cython
+                pp.wheel
+                pp.sphinx
+              ]
+              ))
+              pkgs.freetype
+              pkgs.openssl
+              pkgs.dbus
+              pkgs.dpkg
+              pkgs.SDL2
+              pkgs.portmidi
+            ];
+            nativeBuildInputs = [
+              pkgs.pkg-config
+              pkgs.SDL2_ttf
+              pkgs.SDL2_mixer
+              pkgs.SDL2_image
+            ];
+          }
+        ) 
+      ) sources);
+  in
+    {"default"= "python312-" + first_tag;} // builtins.foldl' (x: y: x // y) {} versions
+
+
---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "Pygame ce overlay";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { self, nixpkgs , flake-utils, ...}@inputs: 
+  let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {inherit system;};
+  in rec {
+    # Packages that append the python version to the previous iteration, so python312-2.5.0 for tag 2.5.0
+    # - default - python312 with latest package
+    packages = import ./default.nix {inherit system pkgs;};
+    devShells.${system}.default = pkgs.mkShell {
+      nativeBuildInputs = with pkgs; [
+        curl
+        jq
+        nurl
+      ];
+    };
+    devShell = self.devShells.${system}.default;
+    overlays.default = final: prev: {
+      pygamepkgs = packages;
+    };
+  };
+}
